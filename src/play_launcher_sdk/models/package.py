5edcadc271b4703af78e3947654a9b0e737769d4
HellLord <ratul.debnath.year@gmail.com>
2026-05-06 09:18:03 +0600

    fix required_client_version

--- src/play_launcher_sdk/models/package.py
+++ src/play_launcher_sdk/models/package.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
+from play_launcher_sdk.types_ import FourPartVersion
+
 from .base import Base
 from .pkg import Pkg
 
@@ -7,4 +9,4 @@ from .pkg import Pkg
 class Package(Base):
     major: Pkg
     patches: list[Pkg]
-    required_client_version: Literal[""]
+    required_client_version: Literal[""] | FourPartVersion
